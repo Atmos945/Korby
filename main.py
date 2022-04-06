6e71bbb3393f5bc0e46ee237f80c220dd3f3bfa0
atmos945 <guillem.george.coubertin@outlook.fr>
2022-04-06 18:52:24 +0200

    trucs cool

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ os.system('cls')
 pygame.init()
 
 # load images
-korby = pygame.image.load("korby droite.png")
-korby2 = pygame.image.load("korby droite 2.png")
-korbyg = pygame.image.load("korby gauche.png")
-korbyg2 = pygame.image.load("korby gauche 2.png")
-bg = pygame.image.load("testbg1.jpg")
+korby_r = pygame.image.load("korby_r.gif")
+korby_l = pygame.image.load("korby_l.gif")
+korby_s = pygame.image.load("korby_s.gif")
+korby_j = pygame.image.load("korby__j.gif")
+bg = pygame.image.load("bg.png")
+# ig = pygame.image.load('ig.png')
+# fg = pygame.image.load('level.png')
 
 # vars definition
 mousedwn = False
@@ -23,7 +25,8 @@ xk = 900
 yk = 600
 xd = 0
 jump=False
-ckorby=korby
+ckorby=korby_r
+
 
 # window properties
 window = pygame.display.set_mode((1800,1000))
@@ -55,18 +58,19 @@ while running == True:
     # keyboard controls
     if event.type==pygame.KEYDOWN:
         if event.key==pygame.K_RIGHT:
-            xk=xk+pas
-            ckorby=korby2
+            xd=xd+pas
+            ckorby=korby_r
         if event.key==pygame.K_LEFT:
             xk=xk-pas
-            ckorby=korbyg
+            ckorby=korby_l
         if event.key==pygame.K_UP:
             jump=True
+            ckroby=korby_j
             for ijp in range(5):
                 yk=yk-jpas/2
-        if event.key==pygame.K_DOWN:
-            if yk>600:
-                yk=yk+pas*2
+        # if event.key==pygame.K_DOWN:
+            # if yk>600:
+                # yk=yk+pas*2
         if event.key==pygame.K_ESCAPE:
             running=False
             pygame.quit()
@@ -74,11 +78,14 @@ while running == True:
     # if event.type==pygame.KEYUP and jump==False:
 
 
+    if event.type==pygame.KEYUP and jump==False:
+        ckroby=korby_s
+
     if event.type==pygame.KEYUP and jump==True:
         while yk<600:
             yk=yk+jpas*2
             window.blit(bg,(0,0))
-            window.blit(korby,(xk,yk))
+            window.blit(ckorby,(xk,yk))
             pygame.display.flip()
         jump = False
 
