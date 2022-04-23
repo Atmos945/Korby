bdaf1d69361c8fc1ba601a42c25d433332bc52db
atmos945 <guillem.george.coubertin@outlook.fr>
2022-04-23 19:23:04 +0200

    integrated foreground (no collisions), fixed animation lagging and y diffs btw walking and idle korby images, beginning of low res displays, + misc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # KORBY  by LastMeridian_, Ordnalessa, Cofordix, and Lelequeen
 # PS: un 20/20 serait le bienvenu :)
-version = 'Version Dev 1.3.7'
+version = 'Version Dev 1.4.0'
 
 
 import pygame, os, random, time, math, ctypes, csv
@@ -8,39 +8,38 @@ import pygame, os, random, time, math, ctypes, csv
 # init
 os.system('cls')
 pygame.init()
+pygame.mixer.init()
 print(version)
 
-# load images
-korby_rframes = [ pygame.image.load("assets/korby_r1.png"), pygame.image.load("assets/korby_r2.png"), pygame.image.load("assets/korby_r3.png"), pygame.image.load("assets/korby_r4.png"), pygame.image.load("assets/korby_r5.png"), pygame.image.load("assets/korby_r6.png"), pygame.image.load("assets/korby_r7.png"), pygame.image.load("assets/korby_r8.png") ]
-korby_lframes = [ pygame.image.load("assets/korby_l1.png"), pygame.image.load("assets/korby_l2.png"), pygame.image.load("assets/korby_l3.png"), pygame.image.load("assets/korby_l4.png"), pygame.image.load("assets/korby_l5.png"), pygame.image.load("assets/korby_l6.png"), pygame.image.load("assets/korby_l7.png"), pygame.image.load("assets/korby_l8.png") ]
-korby_s = pygame.image.load("assets/korby_s.png")
-korby_j = pygame.image.load("assets/korby_j.png")
-korby_f = pygame.image.load("assets/korby_f.png")
-korby_fl = pygame.image.load("assets/korby_fl.png")
-korby_fr = pygame.image.load("assets/korby_fr.png")
-korby_i = pygame.image.load("assets/korby_i.png")
-bg = pygame.image.load("assets/bg.png")
-# ig = pygame.image.load('ig.png')
-# fg = pygame.image.load('level.png')
+# load
+korby_rframes = [ pygame.image.load("assets/1000/korby_r1.png"), pygame.image.load("assets/1000/korby_r2.png"), pygame.image.load("assets/1000/korby_r3.png"), pygame.image.load("assets/1000/korby_r4.png"), pygame.image.load("assets/1000/korby_r5.png"), pygame.image.load("assets/1000/korby_r6.png"), pygame.image.load("assets/1000/korby_r7.png"), pygame.image.load("assets/1000/korby_r8.png") ]
+korby_lframes = [ pygame.image.load("assets/1000/korby_l1.png"), pygame.image.load("assets/1000/korby_l2.png"), pygame.image.load("assets/1000/korby_l3.png"), pygame.image.load("assets/1000/korby_l4.png"), pygame.image.load("assets/1000/korby_l5.png"), pygame.image.load("assets/1000/korby_l6.png"), pygame.image.load("assets/1000/korby_l7.png"), pygame.image.load("assets/1000/korby_l8.png") ]
+korby_s = pygame.image.load("assets/1000/korby_s.png")
+korby_j = pygame.image.load("assets/1000/korby_j.png")
+korby_f = pygame.image.load("assets/1000/korby_f.png")
+korby_fl = pygame.image.load("assets/1000/korby_fl.png")
+korby_fr = pygame.image.load("assets/1000/korby_fr.png")
+korby_i = pygame.image.load("assets/1000/korby_i.png")
+bg = pygame.image.load("assets/1000/bg.png")
+lvl1 = pygame.image.load("assets/1000/lvl1.png")
+# mouse = pygame.image.load("assets/1000/cursor.png")
+pygame.mixer.music.load('assets/korby_metal.wav')
 
-#musiques
-musique_fond = 'KORBY_METAL.wav'
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load(musique_fond)
-pygame.mixer.music.play(-1) 
 
 # vars definition
 user32 = ctypes.windll.user32
 screensize = [user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)]
 print('Screen Resolution ',screensize)
 print("Note that screens with low resolution or high scaling are not supported")
-xk = 900
-yk = 600
+xk = 0
+yk = 550
+yf = 550
 xd = 0
+xm = 0
+ym = 0
 mousedwn = False
 pas = 60
-jpas = 4
+jpas = 40
 # import csv kbctrl
 jump=False
 flight=False
@@ -49,9 +48,9 @@ kccsv = open('kbctrl.csv', 'w', encoding='UTF8')
 kpressed = pygame.key.get_pressed()
 clock= pygame.time.Clock()
 frame = 0
+pause = True
 
 def korby_r_a(): 
-    clock.tick(12)
     global frame
     if frame >= len(korby_rframes):
         frame = 0
@@ -59,7 +58,6 @@ def korby_r_a():
         frame=frame
     korby_r = korby_rframes[frame]
 def korby_l_a(): 
-    clock.tick(12)
     global frame
     if frame >= len(korby_lframes):
         frame = 0
@@ -70,10 +68,12 @@ def korby_l_a():
 
 # window properties
 window = pygame.display.set_mode((1800,1000))
-# pygame.mouse.set_visible(0)
+pygame.mouse.set_visible(0)
 pygame.display.set_icon(korby_i)
-pygame.display.set_caption('SuperKorby 3000 premium deluxe')
+pygame.display.set_caption('SuperKorby 3000 premium deluxe remastered edition +')
 
+
+pygame.mixer.music.play(-1) 
 
 # game loop (whole game, to keep the window open)
 running=True
@@ -83,7 +83,9 @@ while running == True:
             running=False
     window.fill((0,0,0))
     window.blit(bg,(0,0))
-    window.blit(ckorby,(xk,yk))
+    window.blit(ckorby,(900,yk))
+    window.blit(lvl1,(xk,0))
+    # window.blit(mouse,(xm,ym))
     pygame.display.flip()
         
         # detect mouse click
@@ -93,32 +95,32 @@ while running == True:
         mousedwn=False
 
     # curseur custom
-    # if event.type == pygame.MOUSEMOTION and pause == True:
-        # xm=event.pos[0]
-        # ym=event.pos[1]
+    if event.type == pygame.MOUSEMOTION and pause == True:
+        xm=event.pos[0]
+        ym=event.pos[1]
     
     # keyboard controls
     if event.type==pygame.KEYDOWN:
         if event.key==pygame.K_RIGHT:
             if flight==True:
-                    xk=xk+pas/10
+                    xk=xk-pas/10
                     yk=yk+jpas
                     ckorby = korby_fr
             else:
                 frame=frame+1
                 korby_r_a()
-                xk=xk+pas
+                xk=xk-pas
                 ckorby=korby_rframes[frame]
 
         if event.key==pygame.K_LEFT:
             if flight==True:
-                xk=xk-pas/10
+                xk=xk+pas/10
                 yk=yk+jpas
                 ckorby = korby_fl
             else:
                 frame=frame+1
                 korby_l_a()
-                xk=xk-pas
+                xk=xk+pas
                 ckorby=korby_lframes[frame]
 
         if event.key==pygame.K_UP:
@@ -128,10 +130,11 @@ while running == True:
                 yk=yk-jpas*3.5
 
         if event.key==pygame.K_DOWN:
-            # if yk>600:
+            # if yk>yf:
             yk=yk+pas*6
 
-        if event.key==pygame.K_SPACE:
+        if event.key==pygame.K_k:
+            # not detected
             print('K pressed')
             # replace by input()
             kcright = 'pygame.K_RIGHT'
@@ -155,16 +158,17 @@ while running == True:
     if event.type==pygame.KEYUP and flight==True:
         ckorby = korby_f
         
-        if yk<600:
+        if yk<yf:
             yk=yk+jpas*4
             window.blit(bg,(0,0))
-            window.blit(ckorby,(xk,yk))
+            window.blit(ckorby,(900,yk))
+            window.blit(lvl1,(xk,0))
             pygame.display.flip()
 
 
-    if yk>=600:
+    if yk>=yf:
         flight=False
-        yk = 600
+        yk = yf
 
 
 pygame.quit()
